50c34a5ccc75749a0f234ffffb5fab49912b8516
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2024-04-14 23:11:41 +0530

    test: random game generated at server

--- app/page.js
+++ app/page.js
@@ -1,3 +1,17 @@
+"use client"
+
+import { useState } from "react";
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
+
 export default function Home() {
-	return <>Chess</>;
+	const chess = new Chess()
+
+	
+	while (!chess.isGameOver()) {
+	  const moves = chess.moves()
+	  const move = moves[Math.floor(Math.random() * moves.length)]
+	  chess.move(move)
+	}
+	console.log(chess.pgn())
 }
